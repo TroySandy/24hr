ac3fa19d51bc6b9f36ca8b037c32c50d93c97b54
Alexander Mason <tantrex93@gmail.com>
2021-04-20 15:27:27 -0400

    Weather component

--- src/components/OpenWeather.js
+++ src/components/OpenWeather.js
@@ -1,7 +1,54 @@
+import { useEffect, useState } from "react";
+import { Button } from "reactstrap";
+
+function getDirection(angle) {
+  var directions = [
+    "North",
+    "North-West",
+    "West",
+    "South-West",
+    "South",
+    "South-East",
+    "East",
+    "North-East",
+  ];
+  return directions[
+    Math.round(((angle %= 360) < 0 ? angle + 360 : angle) / 45) % 8
+  ];
+}
+
+function tempCelcius(f) {
+  return (f - 32) * (5 / 9);
+}
+
 function OpenWeather(props) {
+  let apiKey = "e6d5c52c7e69d99580386764c2a09e6d";
+  let apiUrl = `//api.openweathermap.org/data/2.5/weather?units=imperial&lat=${props.latitude}&lon=${props.longitude}&appid=${apiKey}`;
+
+  let [temperature, setTemperature] = useState(0);
+  let [tempToggle, setTempToggle] = useState(false);
+  let [city, setCity] = useState("");
+
+  useEffect(() => {
+    fetch(apiUrl)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setTemperature(data.main.temp);
+        setCity(data.name);
+        console.log(data);
+      });
+  }, []);
+
   return (
     <div>
-      {props.longitude} - {props.latitude}
+      <div id="temperature">
+        The weather is currently{" "}
+        {tempToggle ? tempCelcius(temperature).toFixed(2) : temperature}
+        &deg;{tempToggle ? "C" : "F"} in {city}.
+      </div>
+      <Button onClick={() => setTempToggle(!tempToggle)}>Toggle Temp</Button>
     </div>
   );
 }
