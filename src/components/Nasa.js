10da511ed8deb46c15135505733854a08ed8a651
TroySandy <troysandy16@gmail.com>
2021-04-21 09:03:11 -0400

    working project

--- src/components/Nasa.js
+++ src/components/Nasa.js
@@ -1,38 +1,34 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
+import { CardImg } from "reactstrap";
+
 
 function Nasa(props) {
-  
-  const [ imageSrc, setImageSrc ] = useState('');
-  
-  const baseURL = '//api.nasa.gov/planetary/earth/imagery';
-  const key = 'UhGFCeSsF7c3DLnKz50mppSxde9kpgUecJgXeMe9';
-  
+  const [imageSrc, setImageSrc] = useState("");
+
+  const baseURL = "//api.nasa.gov/planetary/earth/imagery";
+  const key = "UhGFCeSsF7c3DLnKz50mppSxde9kpgUecJgXeMe9";
+
   const fetchResults = () => {
-    let url = `${baseURL}?lon=${props.longitude.toFixed(2)}&lat=${props.latitude.toFixed(2)}&dim=0.75&api_key=${key}`;
-    console.log(url);
-    
+    let url = `${baseURL}?lon=${props.longitude.toFixed(
+      2
+    )}&lat=${props.latitude.toFixed(2)}&date=2021-04-01&dim=0.2&api_key=${key}`;
+    // console.log(url);
+
     fetch(url)
-      .then(res => res.blob())
-      .then(blob => {
-        let objURL= URL.createObjectURL(blob)  
-        if(props.longitude !== 0 && props.latitude !== 0){
-          setImageSrc(objURL)
+      .then((res) => res.blob())
+      .then((blob) => {
+        let objURL = URL.createObjectURL(blob);
+        if (props.longitude !== 0 && props.latitude !== 0) {
+          setImageSrc(objURL);
         }
       })
-      .catch(err => console.log(err))
-  }
-  useEffect (() => {
-    fetchResults()
-  }, [props.latitude])
+      .catch((err) => console.log(err));
+  };
+  useEffect(() => {
+    fetchResults();
+  }, [props.latitude]);
 
-  return ( 
-
-  <div className="main">
-    <div className="mainDiv">
-      <img src={imageSrc} width="400px" height="400px"/>
-    </div>
-  </div>
-  )
+  return <CardImg src={imageSrc} alt='Nasa Location Pic'className='cardImg'/>;
 }
 
 export default Nasa;
--- src/components/OpenWeather.js
+++ src/components/OpenWeather.js
@@ -1,24 +1,53 @@
 import { useEffect, useState } from "react";
-import { Button } from "reactstrap";
+import { Card, CardText, CardBody, Button } from "reactstrap";
+import './OpenWeather.css'
+import Nasa from "./Nasa";
 
-function getDirection(angle) {
+function getDirection(angle) { //add north by northwest.... with function stops below...
   var directions = [
     "North",
-    "North-West",
-    "West",
-    "South-West",
-    "South",
-    "South-East",
-    "East",
-    "North-East",
+    'North by East',
+    'North Northeast',
+    'Northeast by North',
+    'Northeast',
+    'Northeast by East',
+    'East-Northeast',
+    'East by North',
+    'East',
+    'East by South',
+    'East-Southeast',
+    'SouthEast by East',
+    'Southeast',
+    'Southeast by South',
+    'South-Southeast',
+    'South by East',
+    'South',
+    'South by West',
+    'South-Southwest',
+    'Southwest by South',
+    'Southwest',
+    'Southwest by West',
+    'West-Southwest',
+    'West by South',
+    'West',
+    'West by North',
+    'West-Northwest',
+    'Northwest by West',
+    'Northwest',
+    'Northwest by North',
+    'North-Northwest',
+    'North by West'
   ];
   return directions[
-    Math.round(((angle %= 360) < 0 ? angle + 360 : angle) / 45) % 8
+    Math.round(((angle %= 360) < 0 ? angle + 360 : angle) / 11.25) % 32
   ];
 }
 
 function tempCelcius(f) {
   return (f - 32) * (5 / 9);
+}
+function windKph(windMph) {
+  return (windMph * 1.60934)
 }
 
 function OpenWeather(props) {
@@ -27,37 +56,44 @@ function OpenWeather(props) {
 
   let [temperature, setTemperature] = useState(0);
   let [tempToggle, setTempToggle] = useState(false);
+  let [windToggle, setWindToggle] = useState(false);
   let [city, setCity] = useState("");
   let [windSpd, setWindSpd] = useState("");
   let [windDir, setWindDir] = useState("");
 
   useEffect(() => {
-    console.log(apiUrl);
+    // console.log(apiUrl);
     fetch(apiUrl)
       .then((res) => {
         return res.json();
       })
       .then((data) => {
+        // console.log(data);
         console.log(data);
-
         setTemperature(data.main.temp);
         setCity(data.name);
         setWindSpd(data.wind.speed);
         setWindDir(getDirection(data.wind.deg));
+        console.log(data.wind.speed, data.wind.deg);
       });
   }, [props.latitude]);
 
   return (
     <div>
-      <div id="temperature">
-        The weather is currently{" "}
-        {tempToggle ? tempCelcius(temperature).toFixed(2) : temperature}
-        &deg;{tempToggle ? "C" : "F"} in {city}.
-      </div>
-      <div id="wind">
-        The wind is {windSpd} MPH {windDir}
-      </div>
-      <Button onClick={() => setTempToggle(!tempToggle)}>Toggle Temp</Button>
+      <Card id='Card'>
+        <Nasa latitude={props.latitude} longitude={props.longitude} />
+        <CardBody>
+          <CardText> The weather is currently{" "}
+              {tempToggle ? tempCelcius(temperature).toFixed(2) : temperature}&deg;{tempToggle ? "C" : "F"} in {city}. The wind is currently blowing {windToggle ? windKph(windSpd).toFixed(2) : windSpd} MPH from the {windDir}</CardText>
+              
+          <Button outline color='info' size='lg' onClick={() => setTempToggle(!tempToggle)}>
+            Toggle C/F
+          </Button>
+          <Button outline color='info' size='lg' onClick={() => setWindToggle(!windToggle)}>
+            Toggle MPH/KPH
+          </Button>
+        </CardBody>
+      </Card>
     </div>
   );
 }
