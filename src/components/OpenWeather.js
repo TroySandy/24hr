205022ad1c8302a0664b85b1b9660ac46bc9e303
Alexander Mason <tantrex93@gmail.com>
2021-04-21 10:10:28 -0400

    added footer & housekeeping

--- src/components/OpenWeather.js
+++ src/components/OpenWeather.js
@@ -1,42 +1,43 @@
 import { useEffect, useState } from "react";
 import { Card, CardText, CardBody, Button } from "reactstrap";
-import './OpenWeather.css'
+import "./OpenWeather.css";
 import Nasa from "./Nasa";
 
-function getDirection(angle) { //add north by northwest.... with function stops below...
+function getDirection(angle) {
+  //add north by northwest.... with function stops below...
   var directions = [
     "North",
-    'North by East',
-    'North Northeast',
-    'Northeast by North',
-    'Northeast',
-    'Northeast by East',
-    'East-Northeast',
-    'East by North',
-    'East',
-    'East by South',
-    'East-Southeast',
-    'SouthEast by East',
-    'Southeast',
-    'Southeast by South',
-    'South-Southeast',
-    'South by East',
-    'South',
-    'South by West',
-    'South-Southwest',
-    'Southwest by South',
-    'Southwest',
-    'Southwest by West',
-    'West-Southwest',
-    'West by South',
-    'West',
-    'West by North',
-    'West-Northwest',
-    'Northwest by West',
-    'Northwest',
-    'Northwest by North',
-    'North-Northwest',
-    'North by West'
+    "North by East",
+    "North Northeast",
+    "Northeast by North",
+    "Northeast",
+    "Northeast by East",
+    "East-Northeast",
+    "East by North",
+    "East",
+    "East by South",
+    "East-Southeast",
+    "SouthEast by East",
+    "Southeast",
+    "Southeast by South",
+    "South-Southeast",
+    "South by East",
+    "South",
+    "South by West",
+    "South-Southwest",
+    "Southwest by South",
+    "Southwest",
+    "Southwest by West",
+    "West-Southwest",
+    "West by South",
+    "West",
+    "West by North",
+    "West-Northwest",
+    "Northwest by West",
+    "Northwest",
+    "Northwest by North",
+    "North-Northwest",
+    "North by West",
   ];
   return directions[
     Math.round(((angle %= 360) < 0 ? angle + 360 : angle) / 11.25) % 32
@@ -47,13 +48,10 @@ function tempCelcius(f) {
   return (f - 32) * (5 / 9);
 }
 function windKph(windMph) {
-  return (windMph * 1.60934)
+  return windMph * 1.60934;
 }
 
 function OpenWeather(props) {
-  let apiKey = "e6d5c52c7e69d99580386764c2a09e6d";
-  let apiUrl = `//api.openweathermap.org/data/2.5/weather?units=imperial&lat=${props.latitude}&lon=${props.longitude}&appid=${apiKey}`;
-
   let [temperature, setTemperature] = useState(0);
   let [tempToggle, setTempToggle] = useState(false);
   let [windToggle, setWindToggle] = useState(false);
@@ -62,13 +60,14 @@ function OpenWeather(props) {
   let [windDir, setWindDir] = useState("");
 
   useEffect(() => {
-    // console.log(apiUrl);
+    let apiKey = "e6d5c52c7e69d99580386764c2a09e6d";
+    let apiUrl = `//api.openweathermap.org/data/2.5/weather?units=imperial&lat=${props.latitude}&lon=${props.longitude}&appid=${apiKey}`;
+
     fetch(apiUrl)
       .then((res) => {
         return res.json();
       })
       .then((data) => {
-        // console.log(data);
         console.log(data);
         setTemperature(data.main.temp);
         setCity(data.name);
@@ -80,18 +79,42 @@ function OpenWeather(props) {
 
   return (
     <div>
-      <Card id='Card'>
+      <Card id="Card">
         <Nasa latitude={props.latitude} longitude={props.longitude} />
         <CardBody>
-          <CardText> The weather is currently{" "}
-              {tempToggle ? tempCelcius(temperature).toFixed(2) : temperature}&deg;{tempToggle ? "C" : "F"} in {city}. The wind is currently blowing {windToggle ? windKph(windSpd).toFixed(2) : windSpd} MPH from the {windDir}</CardText>
-              
-          <Button outline color='info' size='lg' onClick={() => setTempToggle(!tempToggle)}>
-            Toggle C/F
-          </Button>
-          <Button outline color='info' size='lg' onClick={() => setWindToggle(!windToggle)}>
-            Toggle MPH/KPH
-          </Button>
+          <CardText>
+            <p>
+              The weather is currently{" "}
+              {tempToggle ? tempCelcius(temperature).toFixed(2) : temperature}
+              &deg;{tempToggle ? "C" : "F"} in {city}. The wind is currently
+              blowing {windToggle ? windKph(windSpd).toFixed(2) : windSpd}{" "}
+              {windToggle ? "KPH" : "MPH"} from the {windDir}
+            </p>
+            <Button
+              className="mx-1"
+              outline
+              color="info"
+              size="lg"
+              onClick={() => setTempToggle(!tempToggle)}
+            >
+              Toggle {tempToggle ? "Fahrenheit" : "Celcius"}
+            </Button>
+            <Button
+              className="mx-1"
+              outline
+              color="info"
+              size="lg"
+              onClick={() => setWindToggle(!windToggle)}
+            >
+              Toggle {windToggle ? "MPH" : "KPH"}
+            </Button>
+            <hr />
+            <h6>
+              Powered by 🚀<a href="https://api.nasa.gov/">NASA Earth API</a>{" "}
+              &amp; ☁️
+              <a href="https://openweathermap.org/">OpenWeather API</a>
+            </h6>
+          </CardText>
         </CardBody>
       </Card>
     </div>
